db15d431340c5485ff4a70c974deed0f66dd58c9
Prabin Hada <phada@Prabins-MacBook-Pro.local>
2022-01-04 21:32:44 -0500

    updating test

--- test/test.py
+++ test/test.py
@@ -3,14 +3,14 @@ from blockchain.block import Block
 from blockchain.blockchain import Blockchain
 from utils.block_hash import generate_block_id
 
-tx1 = Transaction(sender='prabin', reciever='jdoe', amount=1.0)
-tx2 = Transaction(sender='prabin', reciever='pete', amount=1.2)
+tx1 = Transaction(sender='paul', reciever='jdoe', amount=1.0)
+tx2 = Transaction(sender='peter', reciever='pete', amount=1.2)
 
 transactions = [tx1, tx2]
 
 block1 = Block(0, '', 0, transactions, '')
 block2 = Block(1, '', 0, [Transaction(
-    sender='Jonny', reciever='Matt', amount=2.5)], '')
+    sender='jonny', reciever='matt', amount=2.5)], '')
 
 blockchain = Blockchain([block1, block2], [])
 
